9437ad26224cfb195dbc9dab16b18f270a40f597
Jorge Gonzalez <jglez.cs@gmail.com>
2020-12-14 16:11:02 -0600

    Imported BrowserRouter, Route, Link, and Switch from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+/* 👉 STEP 1 - Import React Router's Router */
+// We can do routing either with the history API using BrowserRouter
+// Or using hashes with the HashRouter
+// We are renaming BrowserRouter to Router for convenience
+// (we can do this because we are only using one router)
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // Importing the top-level component
 import App from './components/App'
@@ -7,10 +13,12 @@ import App from './components/App'
 // Importing the styles
 import './styles.less'
 
-// 👉 STEP 1 - Import React Router's Router
-
 render(
   // Wrap the <App /> in a provider
-  <App />,
+  <Router>
+    <App />
+  </Router>,
+  // Now we can use Router anywhere inside our app because
+  // <App /> is our parent component and data trickles down
   document.querySelector('#root')
 )
